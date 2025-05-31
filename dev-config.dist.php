55c9e17f86b7500d0d8659de3bf86e739b875baa
Mistralys <s.mordziol@gmail.com>
2025-05-31 14:44:03 +0200

    Added configuration template.

---
+++ dev-config.dist.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mistralys\X4;
+
+const X4_GAME_FOLDER = '/path/to/x4';
+
+/**
+ * Absolute path to the folder where the extracted
+ * game files are stored (after unpacking them using
+ * the XRCatTool).
+ *
+ * This is required to extract the data used in
+ * parts of the package.
+ */
+const X4_EXTRACTED_CAT_FILES_FOLDER = '/path/to/folder';
