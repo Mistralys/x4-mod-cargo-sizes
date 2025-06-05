edd607a99c0cb343f3b66d5f47557977598071e7
Mistralys <s.mordziol@gmail.com>
2025-06-05 20:03:49 +0200

    Added error constants.

--- src/Mods/CargoSizesMod/CargoSizeException.php
+++ src/Mods/CargoSizesMod/CargoSizeException.php
@@ -9,4 +9,7 @@ use Mistralys\X4\X4Exception;
 class CargoSizeException extends X4Exception
 {
     public const ERROR_UNHANDLED_SHIP_TYPE = 178001;
+    public const ERROR_MISSING_XML_TAG = 178002;
+    public const ERROR_UNRECOGNIZED_SHIP_SIZE = 178003;
+    public const ERROR_MISSING_RELATIVE_PATH = 178004;
 }
